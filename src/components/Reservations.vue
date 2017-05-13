b0385a852a4708cfd0ca5f77c5fa93afd3443819
IT5706021631069 <poozaza2006@hotmail.com>
2017-05-13 16:56:46 +0700

    popup

--- src/components/Reservations.vue
+++ src/components/Reservations.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="Reservations">
     <div v-for="room in rooms">
-      <div v-if = "room['.key'] == id">
-        จำนวนชั่วโมงที่ต้องการจอง<input type="text" v-model="endtime">
-        <router-link :to="'/profile/' + room['.key'] + '/' + time"><button type="button" name="button" @click="books">จอง</button></router-link>
+      <div class="modal is-active" v-if = "room['.key'] == id">
+        <div class="modal-background"></div>
+          <div class="modal-card">
+            <header class="modal-card-head">
+              <p class="modal-card-title">จำนวนชั่วโมงที่ต้องการจอง</p>
+            </header>
+            <section class="modal-card-body">
+              <a class="button is-success">+</a>
+              <input type="text" v-model="endtime" size="30">
+              <a class="button is-danger">-</a>
+            </section>
+            <footer class="modal-card-foot">
+              <router-link :to="'/profile/' + room['.key'] + '/' + time"><a class="button is-success" @click="books">จอง</a></router-link>
+              <a href="#" class="button">ยกเลิก</a>
+            </footer>
+          </div>
       </div>
     </div>
   </div>
