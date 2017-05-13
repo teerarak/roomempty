d5febe7df1fd80e8830b9837a53763a5b1080519
IT5706021631069 <poozaza2006@hotmail.com>
2017-05-13 18:01:57 +0700

    popup-naja

--- src/components/Reservations.vue
+++ src/components/Reservations.vue
@@ -2,21 +2,33 @@
   <div class="Reservations">
     <div v-for="room in rooms">
       <div class="modal is-active" v-if = "room['.key'] == id">
-        <div class="modal-background"></div>
-          <div class="modal-card">
-            <header class="modal-card-head">
-              <p class="modal-card-title">จำนวนชั่วโมงที่ต้องการจอง</p>
-            </header>
-            <section class="modal-card-body">
-              <a class="button is-success">+</a>
-              <input type="text" v-model="endtime" size="30">
-              <a class="button is-danger">-</a>
-            </section>
-            <footer class="modal-card-foot">
-              <router-link :to="'/profile/' + room['.key'] + '/' + time"><a class="button is-success" @click="books">จอง</a></router-link>
-              <a href="#" class="button">ยกเลิก</a>
-            </footer>
-          </div>
+        <router-link :to="'/'">
+          <div class="modal-background"></div>
+        </router-link>
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">จำนวนชั่วโมงที่ต้องการจอง</p>
+          </header>
+          <section class="modal-card-body">
+            <div class="field has-addons has-addons-centered" >
+              <p class="control">
+                <a class="button is-danger">-</a>
+              </p>
+              <p class="control">
+                <input class="input" type="text" v-model="endtime" placeholder="จำนวนชั่วโมง">
+              </p>
+              <p class="control">
+                <a class="button is-success">+</a>
+              </p>
+            </div>
+          </section>
+          <footer class="modal-card-foot">
+            <router-link :to="'/profile/' + room['.key'] + '/' + time">
+              <a class="button is-success" @click="books" style="padding-left:18px; padding-right:18px;">จอง</a>
+            </router-link>
+            <a href="#" class="button" style="margin-left:15px;">ยกเลิก</a>
+          </footer>
+        </div>
       </div>
     </div>
   </div>
@@ -43,3 +55,15 @@ export default {
   }
 }
 </script>
+<style>
+  @media (min-width:1024px){
+    .modal-card{
+      width: 35vw;
+    }
+  }
+  @media (max-width:1024px){
+    .modal-card{
+      width: 50vw;
+    }
+  }
+</style>
